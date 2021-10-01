bf669df2428f736d36456339397c0be56d4b5bc2
bangerouss <91752655+bangerouss@users.noreply.github.com>
2021-10-02 01:04:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introTitle: '🖐 Helloo ! 🖐',
     introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'Anh biết em rất yêu anh phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thực sự thích anh từ cái nhìn đầu tiên nha :> ',
+    btnYes: 'Có, em thật sự yêu anh Bằng rất nhiều, yêu ơi là yêu ấy 💖💖',
+    btnNo: '💔 Không hề !!! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em yêu anh nhiều thế ??',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    reply: 'Tại vì anh đẹp trai quá đó Ahihiiii ',
+    mess: 'Anh biết mà, Anh cũng yêu Khánh Ly của anh nhiều lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh ngay nha. 💬💬',
+    btnAccept: 'Ok anh yêu 💖',
+    messLink: 'https://m.me/haibang362/'
 }
